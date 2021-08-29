cb71592c1fbaf9920c88949ff3fe28f1e3b59ab3
Knudsen <gknudsen@duqlight.com>
2021-08-29 12:51:17 -0400

    add IConfiguration

--- Startup.cs
+++ Startup.cs
@@ -7,15 +7,21 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace CommanderGQL
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    private readonly IConfiguration ConfigurationBinder;
+
+    public Startup(IConfiguration configuration)
+        {
+            ConfigurationBinder = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
