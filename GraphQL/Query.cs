b6ac9f8abdadb60f81739354c47197a2e0b399aa
Knudsen <gknudsen@duqlight.com>
2021-09-01 11:13:59 -0400

    add projections

--- GraphQL/Query.cs
+++ GraphQL/Query.cs
@@ -8,6 +8,7 @@ namespace CommanderGQL.GraphQL
     public class Query
     {
         [UseDbContext(typeof(AppDbContext))]
+        [UseProjection]
         public IQueryable<Platform> GetPlatform([ScopedService] AppDbContext context)
         {
             return context.Platforms;
